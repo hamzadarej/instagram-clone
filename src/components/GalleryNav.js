d3e778e64ad2aeaff4d222e547cb513d31498630
hamza <darejhamza@gmail.com>
2021-06-09 13:58:00 +0200

    insta-storie-icon

--- src/components/GalleryNav.js
+++ src/components/GalleryNav.js
@@ -1,10 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTh,
+  faFileVideo,
+  faBookmark,
+  faUserTag,
+} from "@fortawesome/free-solid-svg-icons";
 const GalleryNav = () => {
   return (
     <div className="galleryNav">
-      <span>POSTS</span>
-      <span>IGTV</span>
-      <span>SAVED</span>
-      <span>TAGGED</span>
+      <span>
+        <span>
+          <FontAwesomeIcon className="icon" icon={faTh} /> 
+        </span> POSTS
+      </span>
+      <span>
+        <FontAwesomeIcon className="icon" icon={faFileVideo} /> IGTV
+      </span>
+      <span>
+        {" "}
+        <FontAwesomeIcon className="icon" icon={faBookmark} /> SAVED
+      </span>
+      <span>
+        <FontAwesomeIcon className="icon" icon={faUserTag} /> TAGGED
+      </span>
     </div>
   );
 };
