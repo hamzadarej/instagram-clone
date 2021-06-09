b14509382fa557ac38d8fdf75ccf44cf27ee4b1a
hamza <darejhamza@gmail.com>
2021-06-09 15:29:33 +0200

    gallery-img-hover

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,17 +1,25 @@
-const Gallery=({storiesData})=>{
-    const gallery=storiesData.map((obj)=>{
-        const{img}=obj;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faComment } from "@fortawesome/free-solid-svg-icons";
+const Gallery = ({ storiesData }) => {
+  const gallery = storiesData.map((obj) => {
+    const { img, comment, like } = obj;
 
-    
-    
     return (
-       
-        
-<div className="imageG"><img src={img} className="galleryImg"></img></div>
-
-
-    )
-    }) 
-    return <div className="galleries">{gallery}</div>;
+      <div className="imageG">
+        <img src={img} className="galleryImg"></img>
+        <div className="image-hover">
+          <span>
+            <FontAwesomeIcon className="galleryIconHeart" icon={faHeart} /> 
+            <p>{like}</p>
+          </span>
+          <span>
+            <FontAwesomeIcon className="galleryIconComment" icon={faComment} />
+            <p>{comment}</p>
+          </span>
+        </div>
+      </div>
+    );
+  });
+  return <div className="galleries">{gallery}</div>;
 };
 export default Gallery;
