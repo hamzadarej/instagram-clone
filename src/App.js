f9762356531ba3874a5cb7f025beb072c584d8a8
hamza <darejhamza@gmail.com>
2021-06-09 12:14:02 +0200

    insta-gallery

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import Stories from "./components/Stories";
 import storiesData from "./storiesData.json";
 import Gallery from "./components/Gallery";
 import GalleryNav from "./components/GalleryNav";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <Nav Data={Data} />
       <Profile Data={Data} />
      <Stories storiesData={storiesData}/>
      <GalleryNav/>
      <Gallery storiesData={storiesData}/>
 
-    </div>
+    </React.Fragment>
   );
 }
 
